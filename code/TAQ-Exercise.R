9df4ebb96eb5f2aa2fc64a50daa346660adf6113
headsphere <nick@headsphere.net>
2014-12-11 21:57:42 +0000

    Got cleanup code to work

--- code/TAQ-Exercise.R
+++ code/TAQ-Exercise.R
@@ -6,9 +6,10 @@ from = "2014-03-04";
 to = "2014-03-04";
 datasource = "/Users/nick/Documents/RStudioProjects/Dissertation/data/raw";
 datadestination = "/Users/nick/Documents/RStudioProjects/Dissertation/data/xts";
+datadestination_cleaned = "/Users/nick/Documents/RStudioProjects/Dissertation/data/xts_cleaned";
 tickers = c("ACW", "ALG","HZO","ITG")
-tradeColNames <- c("DATE","TIME_M","EX","SYM_ROOT","SYM_SUFFIX","TR_SCOND","SIZE","PRICE","TR_STOPIND","TR_CORR","TR_SEQNUM","TR_SOURCE","TR_RF")
-quoteColNames <- c("DATE","TIME_M","EX","SYM_ROOT","SYM_SUFFIX","BID","BIDSIZ","ASK","ASKSIZ","QU_COND","BIDEX","ASKEX","QU_SEQNUM","NATBBO_IND","NASDBBO_IND","QU_CANCEL","QU_SOURCE")
+tradeColNames <- c("DATE","TIME_M","EX","SYMBOL","SYM_SUFFIX","COND","SIZE","PRICE","TR_STOPIND","CORR","TR_SEQNUM","TR_SOURCE","TR_RF")
+quoteColNames <- c("DATE","TIME_M","EX","SYMBOL","SYM_SUFFIX","BID","BIDSIZ","OFR","OFRSIZ","QU_COND","BIDEX","ASKEX","QU_SEQNUM","NATBBO_IND","NASDBBO_IND","QU_CANCEL","QU_SOURCE")
 
 convert(from, to, datasource, datadestination, trades=TRUE,
         quotes=TRUE,ticker=tickers, dir=FALSE, extension="csv",
@@ -19,19 +20,26 @@ convert(from, to, datasource, datadestination, trades=TRUE,
 #TODO: check the source of the G127 NA errors
 
 options("digits.secs"=3); #Show milliseconds
-sample_tdata = TAQLoad(tickers="HZO", from=from, to=to, trades=T, quotes=F, 
+tdata = TAQLoad(tickers="HZO", from=from, to=to, trades=T, quotes=F, 
                        datasource=datadestination)
 
-sample_qdata = TAQLoad(tickers="HZO", from=from, to=to, trades=F, quotes=T, 
+qdata = TAQLoad(tickers="HZO", from=from, to=to, trades=F, quotes=T, 
                        datasource=datadestination)
-
-#head(sample_tdata)
-
-sample_qdata = sample_qdata[!is.na(index(sample_qdata))]
-sample_tdata = sample_tdata[!is.na(index(sample_tdata))]
 
 #TODO: investigate R timezone handling (low priority)
 
-tqdata = matchTradesQuotes(sample_tdata,sample_qdata);
+#tdata = autoSelectExchangeTrades(tdata = tdata)
+#qdata = autoSelectExchangeQuotes(qdata = qdata)
 
-plot.xts(x = tqdata[, "PRICE", "BID", "OFR"])
+tdata = tradesCleanup(tdataraw = tdata,exchanges="D", report = FALSE)
+qdata = quotesCleanup(qdataraw = qdata,exchanges="T", report = FALSE)
+
+# x[1:(length(x)-window)]
+
+tqdata = matchTradesQuotes(tdata,qdata);
+
+plot(x = tqdata[, "PRICE", "BID", "OFR"])
+
+plot.xts(tqdata[, "PRICE"], type = "bars")
+lines(tqdata[, "OFR"], col="red")
+lines(tqdata[, "BID"], col="green")
