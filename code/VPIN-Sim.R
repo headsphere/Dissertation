401a5950e3d5473c363ce893de0577424fcfae15
headsphere <nick@headsphere.net>
2015-03-30 22:44:45 +0100

    Updates to VPIN simulation

--- code/VPIN-Sim.R
+++ code/VPIN-Sim.R
@@ -1,18 +1,22 @@
-numSim = 100000
+finv <- function(u,p)
+{
+  return (qnorm(u, mean = p))
+}
+
+numSim = 1000
 Vpin = numeric(numSim)
 
-V = 100 #size of volume buckets
-Vb = numeric(V) #buy volume buckets
-Vs = numeric(V) #sell volume buckets
-n = 50 #number of buckets
+V = 50 #size of each volume bucket
+n = 100 #number of volume buckets
+Vbuy = numeric(n) #buy volume buckets
+Vsell = numeric(n) #sell volume buckets
 
-alpha = 0.3 #probability of information event
-delta = 0.5 #probability of bad news event
+alpha = 0.4 #probability of information event
+delta = 0.4 #probability of bad news event
 mu = 10 #arrival rate of informed trader
 epsilon = (V - alpha * mu)/2 #arrival rate of uninformed trader
 
 s = 1
-
 while(s < numSim){
   
   j = 1
@@ -29,41 +33,28 @@ while(s < numSim){
       if(u2 < delta)
       {
         #its a bad news event
-        Vb[j] = finv(u3, epsilon)
-        Vs[j] = finv(u3, mu + epsilon)
+        Vbuy[j] = finv(u3, epsilon)
+        Vsell[j] = finv(u3, mu + epsilon)
       }
       else{
         #its a good news event
-        Vb[j] = finv(u3, mu + epsilon)
-        Vs[j] = finv(u3, epsilon)
+        Vbuy[j] = finv(u3, mu + epsilon)
+        Vsell[j] = finv(u3, epsilon)
       }
     }
     else{
       #no information event
-      Vb[j] = finv(u3, epsilon)
-      Vs[j] = Vb[j]
+      Vbuy[j] = finv(u3, epsilon)
+      Vsell[j] = Vbuy[j]
     }
     
-    j = j + i
+    j = j + 1
   }
   
-  num = 0
-  for(j in 1:n)
-  {
-    num = num + abs(Vb[j] - Vs[j])
-  }
+  Vpin[s] = sum(abs(Vbuy - Vsell)) / sum(Vbuy + Vsell)
   
-  denom = 0
-  for(j in 1:n)
-  {
-    denom = denom + (Vb[j] + Vs[j])
-  }
-  Vpin[s] = num/denom
+  s = s + 1
 }
 meanVpin = sum(Vpin)/numSim
 varVpin =  sum(Vpin^2)/(numSim - 1) - (sum(Vpin)/(numSim * (numSim - 1)))^2
 
-finv <- function(u,p)
-{
-  #TODO - WHAT IS THIS?!?!  
-}
